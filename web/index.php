28149396d79506f1201a38df2bce8d71894abe44
Loïc Payol <loicpayol@gmail.com>
2015-03-17 16:47:16 +0100

    Intégration de MathML

--- web/index.php
+++ web/index.php
@@ -2,22 +2,31 @@
 <?php require '../vendor/autoload.php'; ?>
 <html>
 <head>
-	<meta charset="UTF-8" />
-	<title>Cours</title>
+  <meta charset="UTF-8" />
+  <title>Cours</title>
 
-	<link rel="stylesheet" href="//cdn.jsdelivr.net/highlight.js/8.4/styles/default.min.css">
-	<script src="//cdn.jsdelivr.net/highlight.js/8.4/highlight.min.js"></script>
-	<script>hljs.initHighlightingOnLoad();</script>
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/highlight.js/8.4/styles/default.min.css">
+  <script src="//cdn.jsdelivr.net/highlight.js/8.4/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+  <script type="text/x-mathjax-config">
+  MathJax.Hub.Config({
+    tex2jax: {inlineMath: [['\\(','\\)']]}
+  });
+  </script>
+  <script type="text/javascript"
+    src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+  </script>
+
 
 </head>
 <body>
-	<?php
-		$source = dirname(__FILE__) . '/sources/test.md';
+  <?php
+    $source = dirname(__FILE__) . '/sources/test.md';
 
-		$parser = new \cebe\markdown\GithubMarkdown();
-		$parser->html5 = true;
+    $parser = new \cebe\markdown\GithubMarkdown();
+    $parser->html5 = true;
 
-		echo $parser->parse(file_get_contents($source));
-	?>
+    echo $parser->parse(file_get_contents($source));
+  ?>
 </body>
 </html>
--- web/sources/test.md
+++ web/sources/test.md
@@ -2,12 +2,15 @@
 
 ```java
 public class MyClass {
-    
-    private boolean property;
 
-    public function MyClass()
-    {
-        this.property = false;
-    }
+  private boolean property;
+
+  public function MyClass()
+  {
+    this.property = false;
+  }
+
 }
 ```
+
+Donc, \\(\sum c_iu_i=0 \Rightarrow (c_1+c_2+c_3)v_1+(c_2+c_3)v_2+c_3v_3=0 \Rightarrow c_i=0, \forall i=1, 2, 3.\\).
