bdc40094841209f52a0c3888eab72a1f84919439
Loïc Payol <loicpayol@gmail.com>
2015-03-17 16:36:24 +0100

    Switched to Github markdown

--- web/index.php
+++ web/index.php
@@ -2,13 +2,19 @@
 <?php require '../vendor/autoload.php'; ?>
 <html>
 <head>
+	<meta charset="UTF-8" />
 	<title>Cours</title>
+
+	<link rel="stylesheet" href="//cdn.jsdelivr.net/highlight.js/8.4/styles/default.min.css">
+	<script src="//cdn.jsdelivr.net/highlight.js/8.4/highlight.min.js"></script>
+	<script>hljs.initHighlightingOnLoad();</script>
+
 </head>
 <body>
 	<?php
 		$source = dirname(__FILE__) . '/sources/test.md';
 
-		$parser = new \cebe\markdown\Markdown();
+		$parser = new \cebe\markdown\GithubMarkdown();
 		$parser->html5 = true;
 
 		echo $parser->parse(file_get_contents($source));
