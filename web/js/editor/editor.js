a542c4d34eca12811c8d54c68c046e3cf66fdf9a
armiakrajowa <beaudouink@gmail.com>
2015-03-26 22:56:49 +0100

    Add button Autocompletion function which active / disactive autocompletion. WARNING : by default, autocompletion is off! Add Raccourcis button function! Add localStorage to conserve texte in editor and configuration before a reload (maybe 'for life')

--- web/js/editor/editor.js
+++ web/js/editor/editor.js
@@ -2,6 +2,7 @@ var input = document.getElementById('input');
 input.focus();
 var timer = null;
 var dashMode = false;
+var autocompleteOn = false;
 
 var characterExcept =[
     {
@@ -22,7 +23,7 @@ var characterExcept =[
         charCode : ("'").charCodeAt(0),
         charCodeOppos : ("'").charCodeAt(0) 
     }
-]
+] 
 
 var character = [
     {
@@ -62,6 +63,45 @@ var character = [
     }
 ]
 
+autocompleteButton = document.getElementById("autocomplete");
+autocompleteButton.onclick = function(){
+    if(this.classList.contains("off")){
+        this.classList.remove("off");
+        this.classList.add("on");
+        autocompleteOn = true;
+        localStorage.setItem("autocomplete", "on");
+    }
+    else{
+        this.classList.remove("on");
+        this.classList.add("off");
+        autocompleteOn = false;
+        localStorage.setItem("autocomplete", "false");
+    }
+}
+tabulation = document.getElementById("tabulation");
+tabulation.onclick = function(){
+    if(this.classList.contains("off")){
+        this.classList.remove("off");
+        this.classList.add("on");
+    }
+    else{
+        this.classList.remove("on");
+        this.classList.add("off");
+    }
+}
+raccourcisButton = document.getElementById("raccourcis");
+raccourcisButton.onclick = function(){
+	if(input.value != "" && input.value != raccourcis){
+		if(confirm("Attention, cela va remplacer le texte présent dans l'éditeur, continuer?")){
+			input.value = raccourcis;
+			main();
+		}
+	}
+	else{
+		input.value = raccourcis;
+	}
+}
+
 input.onkeydown = function(e){
 
 	main();
@@ -79,7 +119,7 @@ input.onkeydown = function(e){
 
     console.log(actualKey);
 
-    if(!deleteAutocomplete(this, actualKey, nextChar, previousChar)){
+    if(autocompleteOn && !deleteAutocomplete(this, actualKey, nextChar, previousChar)){
         autocomplete(this, actualKey, selection);
         addCodeQuote(this, actualKey, e, selection, previousChar);
         dashMod(this, actualKey, selection, previousChar);
@@ -92,6 +132,7 @@ input.onkeydown = function(e){
 function main(){
     resetTimer(timer);
     timer = setTimeout(function(){
+        localStorage.setItem("input", input.value);
         appelAjax();
     }, 500);
     input.focus();
@@ -114,78 +155,93 @@ function appelAjax(){
 
 
 window.onload = function(){
-    input.value =   "# Bienvenue dans l'éditeur de Markdown" + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "Ce que vous voyez ci-dessus est un titre de niveau 1 (h1). Le niveau correspond au nombre de # devant le titre"  + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "## Les fonctionnalités de l'éditeur"+ 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "Cet éditeur permet d'éditer un cours et du code avec simplicité avec une multitude de function." + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    " - ### *Liste*" + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "Vous voulez créé une liste? Rien de plus simple, allez a la ligne et ajouté un tiret! L'éditeur ajoutera automatiquement" + 
-                    " l'indentation nécessaire. De plus, si après le premier élément de votre liste vous allez a la ligne, l'éditeur ajoutera" +
-                    " automatiquement l'indentation et le tiret. Allez deux fois a la ligne pour finir votre liste! Essayez : "
-                     + String.fromCharCode(10) + String.fromCharCode(10) +
-                    " - ### *Auto complétion sur les caractères spéciaux*" + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "Si vous souhaitez ajouter, par exemple, des parenthèses autour de cette phrase, vous n'avez qu'a la sélectionner et appuyer sur la touche parenthèse ( ! La parenthèse ouvrante et fermante se placeront automatiquement de chaque coté de votre sélection." + String.fromCharCode(10) + String.fromCharCode(10) +
-                    "Quand vous éditez du code, il est assez ennuyeux d'ajouter sois même les parenthèse, les accolades ou les crochets ouvrants et fermants. Cet éditeur permet donc de compléter automatiquement votre saisie." + String.fromCharCode(10) + String.fromCharCode(10) +
-                    " - ### Insertion de portion de code" + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "Si vous souhaité insérer du code plus facilement, un raccourci essentiel est a connaitre : `ctrl + alt + c`." + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "Ce raccourci ajoutera automatiquement les caractères spéciaux nécessaires à l'insertion de portion de code sous cette forme :" + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "```<nom du langage>" + 
-                    String.fromCharCode(10) +
-                    "<Votre code>" + 
-                    String.fromCharCode(10) +
-                    "```" + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "Il est également possible d’insérer du code dans une phrase avec `alt + c` qui ajoutera les caractères necessaires." + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "Si votre code est ** `ce mot` ** alors sélectionnez le et faite `alt + c`." +
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "## Les fonctionnalités du Markdown" + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "Le Markdown permet de nombreuse chose que je vais répertorier dans le tableau si dessous : " + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    "Symbole             |   Fonctionnalité" + 
-                    String.fromCharCode(10) +
-                    ":-------------------|:--------------------: " + 
-                    String.fromCharCode(10) +
-                    "  `*texte*`         |  *Mettre en italique*" + 
-                    String.fromCharCode(10) +
-                    "  `**texte**`       |  **Mettre en gras**" + 
-                    String.fromCharCode(10) +
-                    " - Martin           |  Faire une liste" + 
-                    String.fromCharCode(10) +
-                    "   #Kevin           |  Titre niveau 1 (h1)" + 
-                    String.fromCharCode(10) +
-                    "   ##Kevin          |  Titre niveau 2 (h2)" + 
-                    String.fromCharCode(10) +
-                    "  ###Kevin          |  Titre niveau 3 (h3)" + 
-                    String.fromCharCode(10) +
-                    "   `<u>`            |  <u>Souligé" + 
-                    String.fromCharCode(10) +
-                    "   `<s>`            |  <s>Rayé" + 
-                    String.fromCharCode(10) +
-                    "   `<q>`            |  <q>Citation" + 
-                    String.fromCharCode(10) +
-                    "    ```             | `Encadrer du code`" + 
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    String.fromCharCode(10) + String.fromCharCode(10) +
-                    String.fromCharCode(10) + String.fromCharCode(10);
 
+    if(localStorage.getItem("autocomplete") == "on"){
+        autocompleteButton = document.getElementById("autocomplete");
+        autocompleteButton.classList.remove("off");
+        autocompleteButton.classList.add("on");
+        autocompleteOn = true;
+    }
+
+
+    if(localStorage.getItem("input") !== "" || localStorage.getItem("input") !== null){
+        input.value = localStorage.getItem("input");
+    }
+    else{
+        input.value = raccourcis;
+    }
     appelAjax();
 }
+
+raccourcis ="# Bienvenue dans l'éditeur de Markdown" + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "Ce que vous voyez ci-dessus est un titre de niveau 1 (h1). Le niveau correspond au nombre de # devant le titre"  + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "## Les fonctionnalités de l'éditeur"+ 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "Cet éditeur permet d'éditer un cours et du code avec simplicité avec une multitude de function." + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            " - ### *Liste*" + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "Vous voulez créé une liste? Rien de plus simple, allez a la ligne et ajouté un tiret! L'éditeur ajoutera automatiquement" + 
+            " l'indentation nécessaire. De plus, si après le premier élément de votre liste vous allez a la ligne, l'éditeur ajoutera" +
+            " automatiquement l'indentation et le tiret. Allez deux fois a la ligne pour finir votre liste! Essayez : "
+             + String.fromCharCode(10) + String.fromCharCode(10) +
+            " - ### *Auto complétion sur les caractères spéciaux*" + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "Si vous souhaitez ajouter, par exemple, des parenthèses autour de cette phrase, vous n'avez qu'a la sélectionner et appuyer sur la touche parenthèse ( ! La parenthèse ouvrante et fermante se placeront automatiquement de chaque coté de votre sélection." + String.fromCharCode(10) + String.fromCharCode(10) +
+            "Quand vous éditez du code, il est assez ennuyeux d'ajouter sois même les parenthèse, les accolades ou les crochets ouvrants et fermants. Cet éditeur permet donc de compléter automatiquement votre saisie." + String.fromCharCode(10) + String.fromCharCode(10) +
+            " - ### Insertion de portion de code" + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "Si vous souhaité insérer du code plus facilement, un raccourci essentiel est a connaitre : `ctrl + alt + c`." + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "Ce raccourci ajoutera automatiquement les caractères spéciaux nécessaires à l'insertion de portion de code sous cette forme :" + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "```<nom du langage>" + 
+            String.fromCharCode(10) +
+            "<Votre code>" + 
+            String.fromCharCode(10) +
+            "```" + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "Il est également possible d’insérer du code dans une phrase avec `alt + c` qui ajoutera les caractères necessaires." + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "Si votre code est ** `ce mot` ** alors sélectionnez le et faite `alt + c`." +
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "## Les fonctionnalités du Markdown" + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "Le Markdown permet de nombreuse chose que je vais répertorier dans le tableau si dessous : " + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            "Symbole             |   Fonctionnalité" + 
+            String.fromCharCode(10) +
+            ":-------------------|:--------------------: " + 
+            String.fromCharCode(10) +
+            "  `*texte*`         |  *Mettre en italique*" + 
+            String.fromCharCode(10) +
+            "  `**texte**`       |  **Mettre en gras**" + 
+            String.fromCharCode(10) +
+            " - Martin           |  Faire une liste" + 
+            String.fromCharCode(10) +
+            "   #Kevin           |  Titre niveau 1 (h1)" + 
+            String.fromCharCode(10) +
+            "   ##Kevin          |  Titre niveau 2 (h2)" + 
+            String.fromCharCode(10) +
+            "  ###Kevin          |  Titre niveau 3 (h3)" + 
+            String.fromCharCode(10) +
+            "   `<u>`            |  <u>Souligé" + 
+            String.fromCharCode(10) +
+            "   `<s>`            |  <s>Rayé" + 
+            String.fromCharCode(10) +
+            "   `<q>`            |  <q>Citation" + 
+            String.fromCharCode(10) +
+            "    ```             | `Encadrer du code`" + 
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            String.fromCharCode(10) + String.fromCharCode(10) +
+            String.fromCharCode(10) + String.fromCharCode(10);
